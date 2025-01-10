029cd8fe6056ec0ac4245e87638123e67942a927
Arkadii <arkadii.malygin@gmail.com>
2025-01-10 19:37:13 +0200

    appwrite creation process

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import AuthForm from '@/components/AuthForm'
 
-const SignIn = () => {
-    return (
-        <div>
-            SignIn
-        </div>
-    );
-};
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  )
+}
 
-export default SignIn;
+export default SignUp
